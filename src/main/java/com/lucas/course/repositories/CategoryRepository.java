2d3575fadb30ea2fa4101e19d466881b3184bbac
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-05 20:46:23 -0300

    Category

---
+++ src/main/java/com/lucas/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.course.repositories;
+
+import com.lucas.course.entities.Category;
+import com.lucas.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
