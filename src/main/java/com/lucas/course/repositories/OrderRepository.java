626100db1d462ad38552b63cad2e818e2df87c50
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-05 19:51:22 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/lucas/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.course.repositories;
+
+import com.lucas.course.entities.Order;
+import com.lucas.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
